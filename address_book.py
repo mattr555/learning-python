3088cce0970c89d6906256e8a4c09874fcf9b7af
Matthew Ramina <mattramina@gmail.com>
2013-12-10 18:18:21 -0500

    added example address book

---
+++ address_book.py
@@ -0,0 +1,11 @@
+#Matt Ramina
+#Address book
+
+first_name = input('Enter the first name: ')
+last_name = input('Enter the last name: ')
+email = input('Enter the email: ')
+phone_number = input('Enter the phone number: ')
+
+print(last_name + ', ' + first_name)
+print('Email: ' + email)
+print('Phone: ' + phone_number)
